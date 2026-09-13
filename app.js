9406234fa4aa40b8d5cb5ddab0516ba29adb43c0
Md Kamal Uddin <60796717+KamalPixi@users.noreply.github.com>
2026-09-13 21:17:49 +0800

    Update response message in root route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World! My first Node.js app is running. CI/CD 2');
+  res.send('Hello World! My first Node.js app is running. CI/CD 3');
 });
 
 // Start the server
